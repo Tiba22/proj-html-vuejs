f3afa2e7ce98ffb111bf06fd9b72db74df2387a9
Alessandro Tibaldo <aletibo@gmail.com>
2021-04-28 11:42:15 +0200

    Altre sezioni completate

--- js/script.js
+++ js/script.js
@@ -48,12 +48,60 @@ function initVue() {
           info:'SATISFACTION RATE'
         }
       ],
+
       checks: [
         'Select & customize courses to your preferences','Change the tutor and make arrangements',
         'Participate in events to join others',
         'Get the desired certificate delivered at house'
       ],
-      
+
+      courses:[
+        {
+          img: 'finance-course-thumbnail-04-480x298.jpg',
+          price: 'Free',
+          info: 'Personal Finance: Financial Security Thinking & Principles',
+          infoCourse: ['2 Lessons','89 Students'],
+        },
+        {
+          img: 'course-02-480x298.jpg',
+          price: '$40.00',
+          info: 'Learning to Write as a Professional Author',
+          infoCourse: ['20 Lessons','50 Students'],
+        },
+        {
+          img: 'stock-full-hd-03-480x298.jpg',
+          price: 'Free',
+          info: 'Customer-centric Info-tech Strategies',
+          infoCourse: ['24 Lessons','802 Students'],
+        },
+        {
+          img: 'stock-full-hd-04-480x298.jpg',
+          price: '$19.00',
+          info: 'Open Programming Courses for everyone: Python',
+          infoCourse: ['17 Lessons','62 Students'],
+        },
+        {
+          img: 'stock-full-hd-06-480x298.jpg',
+          price: '$26.00',
+          info: 'Academix Listening and Note-taking ',
+          infoCourse: ['14 Lessons','67 Students'],
+        },
+        {
+          img: 'course-featured-image-01-480x298.jpg',
+          price: '$39.00',
+          info: 'Master jQuery in a short Period of Time',
+          infoCourse: ['6 Lessons','51 Students'],
+        }
+      ],
+      clients: [
+        'client-logo-colored-01.png',
+        'client-logo-colored-02.png',
+        'client-logo-colored-03.png',
+        'client-logo-colored-04.png',
+        'client-logo-colored-05.png',
+        'client-logo-colored-06.png'
+      ],
+
       counter: null,
     },
 
